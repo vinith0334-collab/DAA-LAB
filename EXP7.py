96b33a28242c9f5eee6e1279926972f62ab74cfd
vinith0334-collab <vinith0334@gmail.com>
2026-08-28 13:54:56 +0530

    Create EXP7.py

---
+++ EXP7.py
@@ -0,0 +1,78 @@
+def is_safe(board, row, col): 
+    for prev_row in range(row): 
+        placed = board[prev_row] 
+        if placed == col:  # Same column 
+            return False 
+        if abs(prev_row - row) == abs(placed - col):  # Diagonal 
+            return False 
+    return True 
+  
+def solve_n_queens(n): 
+    board     = [-1] * n 
+    solutions = [] 
+    backtrack_count = [0] 
+  
+    def backtrack(row): 
+        if row == n: 
+            solutions.append(board[:]) 
+            return 
+        for col in range(n): 
+            if is_safe(board, row, col): 
+                board[row] = col 
+                backtrack(row + 1) 
+                board[row] = -1  # Undo 
+                backtrack_count[0] += 1 
+  
+    backtrack(0) 
+    return solutions, backtrack_count[0] 
+  
+def display_board(solution, n): 
+    print('  +' + '---+' * n) 
+    for row in range(n): 
+        print('  |', end='') 
+        for col in range(n): 
+            if solution[row] == col: 
+                print(' Q |', end='') 
+            else: 
+                print(' . |', end='') 
+        print() 
+        print('  +' + '---+' * n) 
+  
+# --- Solve for N=4 (show all) and N=8 (count only) --- 
+for n in [4, 6, 8]: 
+    solutions, backtracks = solve_n_queens(n) 
+    print(f'N={n}: {len(solutions)} solutions, {backtracks} backtracks') 
+    if n == 4: 
+        print(f'\n  All solutions for {n}-Queens:') 
+        for i, sol in enumerate(solutions, 1): 
+            print(f'\n  Solution {i}: {sol}') 
+            display_board(sol, n) 
+
+OUTPUT:
+N=4: 2 solutions, 16 backtracks
+
+  All solutions for 4-Queens:
+
+  Solution 1: [1, 3, 0, 2]
+  +---+---+---+---+
+  | . | Q | . | . |
+  +---+---+---+---+
+  | . | . | . | Q |
+  +---+---+---+---+
+  | Q | . | . | . |
+  +---+---+---+---+
+  | . | . | Q | . |
+  +---+---+---+---+
+
+  Solution 2: [2, 0, 3, 1]
+  +---+---+---+---+
+  | . | . | Q | . |
+  +---+---+---+---+
+  | Q | . | . | . |
+  +---+---+---+---+
+  | . | . | . | Q |
+  +---+---+---+---+
+  | . | Q | . | . |
+  +---+---+---+---+
+N=6: 4 solutions, 152 backtracks
+N=8: 92 solutions, 2056 backtracks
